381dd6ae15b173c6017aacaf35738dff712efd98
JazzyJakey <66395612+JazzyJakey@users.noreply.github.com>
2021-03-12 12:35:12 -0500

    Created mobile navbar for mobile portrait mode.Fonts need cleaned up abut all n' all, she's a beaut Clark

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Footer from "../src/component/Footer";
 import Navbar from "../src/component/Navbar/Navbar";
+import MobileNavbar from "../src/component/Navbar/MobileNavbar"
+
 import './App.css';
 
 import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
@@ -21,10 +23,17 @@ const theme = createMuiTheme(); {
   };
 }
 
+theme.typography.h6 = {
+  fontFamily: "KOJ, sans-serif",
+  fontSize: '1.5rem',
+  lineHeight: '1.5',
+};
+
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
     <div>
+      <MobileNavbar/>
       <Navbar />
       <Home />
       <Footer />

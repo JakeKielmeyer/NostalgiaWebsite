200220e892ba5d81a5d3c888942cafce35cab80f
JakeKielmeyer <66395612+JakeKielmeyer@users.noreply.github.com>
2021-04-02 12:06:32 -0400

    I deleted the old home files and renamed the new home sections. Fixed the routing issue so now when the app is loaded it goes to the home page (#28)
    Co-authored-by: JazzyJakey <66395612+JazzyJakey@users.noreply.github.com>

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,23 +1,47 @@
-import React from "react";
+import React,{useState}  from "react";
 
 //mui stuff
 import withStyles from "@material-ui/core/styles/withStyles";
+import GridList from "@material-ui/core/GridList";
 
-// Compenents
-
-// Page Sections
-import New from "../NewHome/New";
+// Components
+import About from "./About";
+import Photos from "./Photo";
+import Music from "./Music";
+import Video from "./Video";
+import Puzzle from "./Puzzle";
+import Carousel from "react-material-ui-carousel";
+import Banner from "../../component/banner";
+import Call from "../../component/callToAction";
+import Features from "./Features";
 
 const styles = {
 };
 
-const Home = (props) => {
+function New(props) {
   const { classes } = props;
   return (
-<div>
-  <New/>
-</div>
+
+      <div>
+        <Banner/>
+        <About/>
+        <Call/>
+          <Carousel
+            autoPlay={false}
+            fullHeightHover={false}
+            animation={"fade"}
+            timeout={12}
+            navButtonsAlwaysVisible={true}
+            indicatorContainerProps={{style: {  },}}>
+            
+            <Features/> 
+            <Photos/>
+            <Music/>          
+            <Video/>
+            <Puzzle/>
+          </Carousel>
+        </div>
   );
 };
 
-export default withStyles(styles)(Home);
+export default withStyles(styles)(New);
